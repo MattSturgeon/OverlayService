c567949a2c868d34771b6917cb87b6e0c3d0c1a5
Matt Sturgeon <matt@sturgeon.me.uk>
2022-08-27 22:05:29 +0100

    src and data directories

--- install.sh
+++ install.sh
@@ -3,19 +3,19 @@
 echo "Installing server binary to /usr/bin/overlay-service"
 mkdir -p /usr/bin
 rm -rf /usr/bin/overlay-service
-ln -s "$(realpath ./builddir/overlay-service)" /usr/bin/overlay-service
+ln -s "$(realpath ./builddir/src/overlay-service)" /usr/bin/overlay-service
 
 echo "Installing dbus permissions config to /usr/share/dbus-1/system.d/org.github.MattSturgeon.OverlayService.conf"
 mkdir -p /usr/share/dbus-1/system.d/
 rm -rf /usr/share/dbus-1/system.d/org.github.MattSturgeon.OverlayService.conf
-ln -s "$(realpath ./dbus.conf)" /usr/share/dbus-1/system.d/org.github.MattSturgeon.OverlayService.conf
+ln -s "$(realpath ./data/dbus.conf)" /usr/share/dbus-1/system.d/org.github.MattSturgeon.OverlayService.conf
 
 echo "Installing dbus service to /usr/share/dbus-1/system-services/org.github.MattSturgeon.OverlayService.service"
 mkdir -p /usr/share/dbus-1/system-services
 rm -rf /usr/share/dbus-1/system-services/org.github.MattSturgeon.OverlayService.service
-ln -s "$(realpath ./dbus.service)" /usr/share/dbus-1/system-services/org.github.MattSturgeon.OverlayService.service
+ln -s "$(realpath ./data/dbus.service)" /usr/share/dbus-1/system-services/org.github.MattSturgeon.OverlayService.service
 
 echo "Installing systemd service to /usr/lib/systemd/system/overlay.service"
 mkdir -p /usr/lib/systemd/system
 rm -rf /usr/lib/systemd/system/overlay.service
-ln -s "$(realpath ./systemd.service)" /usr/lib/systemd/system/overlay.service
+ln -s "$(realpath ./data/systemd.service)" /usr/lib/systemd/system/overlay.service
